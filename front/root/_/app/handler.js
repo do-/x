09c24f4b253b77e17c9df746a4cb1c1cadd47a70
Dmitry Ovsyanko <do_@pochta.ru>
2019-05-01 13:57:54 +0300

    handler.js cleaned up

--- front/root/_/app/handler.js
+++ front/root/_/app/handler.js
@@ -1,34 +1,15 @@
-if (window.__LOGOUT__) delete window.__LOGOUT__
+(function () {
 
-function get_default_url () {
+    clearTimeout (window.alarm)
 
-    return '/users'
+    if (!$_USER) return show_block ('login')
 
-}
+    let [type, id] = location.pathname.split ('/').filter ((i) => i)
 
-function setup_request () {
+    if (!type) return redirect (window.name = '/users')
 
-    var parts = location.pathname.split ('/').filter (function (i) {return i})
-    
-    if (!parts.length && $_USER && $_USER.role) return redirect (window.name = get_default_url ())
+    $_REQUEST = {type, id}
 
-    $_REQUEST = {type: parts [0]}
-    
-    if (parts [1]) $_REQUEST.id = parts [1]
+    show_block ('main')
 
-}
-
-clearTimeout (window.alarm)
-
-$_SESSION.beforeExpiry ($_SESSION.keepAlive)
-
-window.addEventListener ('storage', $_SESSION.closeAllOnLogout)
-
-if ($_USER && $_USER.opt && $_USER.opt.no_tabs) openTab = function (url, name) {
-    window.name = name || url
-    location = url
-}
-
-setup_request ()
-
-show_block ($_USER ? 'main' : 'login')
+})()
--- front/root/_/app/js/data/main.js
+++ front/root/_/app/js/data/main.js
@@ -1,3 +1,16 @@
 ////////////////////////////////////////////////////////////////////////////////
 
-$_GET.main = async function (o) {}
+$_GET.main = async function (o) {
+
+    if (window.__LOGOUT__) delete window.__LOGOUT__
+
+    $_SESSION.beforeExpiry ($_SESSION.keepAlive)
+
+    window.addEventListener ('storage', $_SESSION.closeAllOnLogout)
+
+    if ($_USER.opt.no_tabs) open_tab = openTab = function (url, name) {
+        window.name = name || url
+        location = url
+    }
+
+}
