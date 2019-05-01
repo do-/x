3c79d358611b1df653ab22471f036badddae68e0
Dmitry Ovsyanko <do_@pochta.ru>
2019-05-01 15:57:07 +0300

    user_password refactored

--- front/root/_/app/js/view/user_password.js
+++ front/root/_/app/js/view/user_password.js
@@ -1,33 +1,20 @@
 $_DRAW.user_password = async function (data) {
 
-    (await use.jq ('user_password')).w2popup ('open', {
-
-        width  : 230,
-        height : 195,
-
-        title   : 'Смена пароля',
-
-        onOpen: function (e) {
-
-            e.done (function () {               
-            
-                $('#w2ui-popup .w2ui-form').w2reform ({
-
-                    name: 'passwordForm',
-
-                    record: {},
-
-                    fields : [
-                        {name: 'p1', type: 'password'},
-                        {name: 'p2', type: 'password'},
-                    ],
-                    
-                })
-                
-            })
-            
-        }
+    (await use.jq ('user_password')).w2uppop ({}, function () {
         
+        $('#w2ui-popup .w2ui-form').w2reform ({
+
+            name: 'passwordForm',
+
+            record: {},
+
+            fields : [
+                {name: 'p1', type: 'password'},
+                {name: 'p2', type: 'password'},
+            ],
+
+        })
+
     })
-    
+
 }
