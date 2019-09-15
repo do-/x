7a6ea63d40992e2ea76deff8b9b5b8484ecc1d5e
Dmitry Ovsyanko <do_@pochta.ru>
2019-09-15 12:47:04 +0300

    ssh_batch validation

--- back/lib/Content/ssh_batch.js
+++ back/lib/Content/ssh_batch.js
@@ -7,6 +7,10 @@ do_run_ssh_batch:
     async function () {
     
     	this.rq.id = this.uuid
+    	
+    	if (this.rq.cmd == null) throw '-32602 Empty cmd'
+    	if (this.rq.addr == null) throw '-32602 Empty addr'
+    	if (!Array.isArray (this.rq.addr)) throw '-32602 The addr parameter must be an array'
 
 		return await this.fork ({type: 'ssh_commands', action: 'create'}, {data: {
 			cmd  : this.rq.cmd,
