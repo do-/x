37fcc6712b73f314dff41b8925b06684dfd34818
Dmitry Ovsyanko <do_@pochta.ru>
2019-09-15 15:10:08 +0300

    request id validation gone from core to app

--- back/lib/Content/ssh_batch.js
+++ back/lib/Content/ssh_batch.js
@@ -8,6 +8,7 @@ do_run_ssh_batch:
     
     	this.rq.id = this.uuid
     	
+        if (!/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test (this.rq.id)) throw "-32602 Request id is not a UUID"
     	if (this.rq.cmd == null) throw '-32602 Empty cmd'
     	if (this.rq.addr == null) throw '-32602 Empty addr'
     	if (!Array.isArray (this.rq.addr)) throw '-32602 The addr parameter must be an array'
