5db56170f000391e4e2751aaaba3132d0650df04
Dmitry Ovsyanko <do_@pochta.ru>
2019-10-05 12:47:46 +0300

    decomposition: WwebUiBackend handler extracted

--- back/lib/Content.js
+++ back/lib/Content.js
@@ -1,44 +1,11 @@
 const Dia = require ('./Ext/Dia/Dia.js')
 const HTTPJsonRpc = require ('./Ext/Dia/Content/Handler/HTTP/JsonRpc.js')
 const HTTPStatic = require ('./Ext/Dia/Content/Handler/HTTP/Static.js')
-const CachedCookieSession = require ('./Ext/Dia/Content/Handler/HTTP/Session/CachedCookieSession.js')
 
 const Base = require ('./Content/Handler/Base.js') 
 
-let handler = {}
-
-handler._back = class extends Dia.HTTP.Handler {
-
-    constructor (o) {
-    	super (o)
-    	this.import (Base, ['get_method_name', 'fork'])
-    }
-
-    check () {
-        super.check ()
-        let m = this.http.request.method
-        if (m != 'POST') throw '405 No ' + m + 's please'
-    }
-
-    get_session () {
-
-    	let h = this
-
-    	return new CachedCookieSession (h, {
-    		sessions:    h.pools.sessions,
-    		cookie_name: h.conf.auth.sessions.cookie_name || 'sid',
-    	})
-
-    }
-    
-    is_anonymous () {
-        return this.rq.type == 'sessions' && this.rq.action == 'create'
-    }
-    
-    w2ui_filter () {
-        return new (require ('./Ext/DiaW2ui/Filter.js')) (this.rq)
-    }
-    	
+let handler = {
+	_back: require ('./Content/Handler/WebUiBackend.js'),
 }
 
 function http_listener (conf) {
---
+++ back/lib/Content/Handler/WebUiBackend.js
@@ -0,0 +1,35 @@
+const Dia = require ('../../Ext/Dia/Dia.js')
+
+module.exports = class extends Dia.HTTP.Handler {
+
+    constructor (o) {
+    	super (o)
+    	this.import ((require ('./Base')), ['get_log_banner', 'get_method_name', 'fork'])
+    }
+    
+    check () {
+        super.check ()
+        let m = this.http.request.method
+        if (m != 'POST') throw '405 No ' + m + 's please'
+    }
+
+    get_session () {
+
+    	let h = this
+
+    	return new (require ('../../Ext/Dia/Content/Handler/HTTP/Session/CachedCookieSession.js')) (h, {
+    		sessions:    h.pools.sessions,
+    		cookie_name: h.conf.auth.sessions.cookie_name || 'sid',
+    	})
+
+    }
+    
+    is_anonymous () {
+        return this.rq.type == 'sessions' && this.rq.action == 'create'
+    }
+    
+    w2ui_filter () {
+        return new (require ('../../Ext/DiaW2ui/Filter.js')) (this.rq)
+    }
+
+}
