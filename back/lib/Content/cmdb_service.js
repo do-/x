b3738daa89313bd23748cbe81736bcfc9ed714b9
Dmitry Ovsyanko <do_@pochta.ru>
2019-10-12 15:24:53 +0300

    logging http traffic

--- back/lib/Content/cmdb_service.js
+++ back/lib/Content/cmdb_service.js
@@ -31,6 +31,10 @@ do_call_cmdb_service:
 			for (let k in data) d [log.fields [k]] = data [k]
 			return db.update (log.table, d)
 		}
+		
+		let sniff = (rq, rp, body) => {
+			darn (`${this.uuid}: ${JSON.stringify (rq._header)} ${json} -> ${JSON.stringify (rp.headers)} ${body}`)
+		}
 				
 		try {
 
@@ -43,7 +47,13 @@ do_call_cmdb_service:
 					error     : null,
 				})
 
-				http.request (url, o, rp => {
+				let rq = http.request (url, o, rp => {
+				
+					let body = ''
+					
+					rp.setEncoding ('utf8')
+					rp.on ('data', s => body += s)
+					rp.on ('end', () => sniff (rq, rp, body))
 
 					let code = rp.statusCode; if (code == 200) return ok ()
 
