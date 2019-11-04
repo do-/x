486ba680477f0adbf15a1841f4579b565c02e539
Dmitry Ovsyanko <do_@pochta.ru>
2019-11-04 11:40:57 +0300

    boot logging++

--- back/lib/index.js
+++ back/lib/index.js
@@ -4,8 +4,13 @@ const http_router  = new (require ('./HttpRouter.js')) (conf)
 ;(async () => {
 
 	try {
+	
 		await conf.init ()
+        console.log ('Configuration loaded OK')
+        
 		await http_router.init ()
+        console.log ('Listening to HTTP on ' + http_router._._connectionKey)
+		
 	}
 	catch (e) {
 		return darn (['Failed to initialize', e])
