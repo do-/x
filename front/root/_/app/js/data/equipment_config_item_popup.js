fab5cc55de653defffc165730428ef9748f6d5a8
Dmitry Ovsyanko <do_@pochta.ru>
2019-11-30 10:47:38 +0300

    equipment_config_item_popup show response

--- front/root/_/app/js/data/equipment_config_item_popup.js
+++ front/root/_/app/js/data/equipment_config_item_popup.js
@@ -1,7 +1,29 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 $_GET.equipment_config_item_popup = async function (o) {
+    
+    let data = clone (o); for (let k of ['json', 'error']) data [k] = (s => {	
+    
+    	try {
 
-    return {json: JSON.stringify (JSON.parse (o.json), null, 2)}
+    		return JSON.stringify (JSON.parse (s), null, 2)
+
+    	}
+    	catch (x) {
+
+    		return s
+
+    	}
+    
+    }) (o [k])
+
+    if (data.ts_error) {
+    	data.ts_finish = data.ts_error
+    }
+    else {
+    	data.error = '200 OK'
+    }
+
+    return data
     
 }
