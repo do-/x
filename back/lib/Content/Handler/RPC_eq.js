5a1e8848590595eeb5fc812567733d7cb9270208
Dmitry Ovsyanko <do_@pochta.ru>
2019-12-03 20:38:04 +0300

    code typo (case) FIX

--- back/lib/Content/Handler/RPC_eq.js
+++ back/lib/Content/Handler/RPC_eq.js
@@ -1,4 +1,4 @@
-module.exports = class extends require ('./Rpc.js') {
+module.exports = class extends require ('./RPC.js') {
     
     get_session () {
     	return new (require ('./HTTP/EqSession.js')) (this)
